12f828ba019ed4de22bdb472b3bae83ef5957dd8
Sebastin Santy <sebastinssanty@gmail.com>
2017-01-17 17:35:10 +0530

    Export queryset to xls

--- quickreg/admin.py
+++ quickreg/admin.py
@@ -1,66 +1,75 @@
 from django.contrib import admin
 from .models import *
+from django.http import HttpResponse
 
-# def export_xls(modeladmin, request, queryset):
-# 	# print(queryset)
-#     import xlwt
-#     response = HttpResponse(content_type='application/ms-excel')
-#     response['Content-Disposition'] = 'attachment; filename=profile.xls'
-#     wb = xlwt.Workbook(encoding='utf-8')
-#     ws = wb.add_sheet("Profile")
+def export_xls(modeladmin, request, queryset):
+    import xlwt
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=quick-reg.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet("Profile")
     
-#     row_num = 0
+    row_num = 0
     
-#     columns = [
-#         (u"Name", 6000),
-#         (u"Email", 4000),
-#         (u"Mobile", 6000),
-#         (u"Institute", 10000),
-#         (u"Gender", 500),
-#         (u"DOB", 2000),
-#         (u"Year", 1000),
-#         (u"Events", 8000),
-#         (u"Workshop", 8000),
-#     ]
+    columns = [
+        (u"Name", 6000),
+        (u"Email", 4000),
+        (u"Mobile", 6000),
+        (u"Institute", 10000),
+        (u"Gender", 500),
+        (u"Year", 1000),
+        (u"Panel Design", 5000),
+        (u"Panel Elixir", 5000),
+        (u"Panel Roboficial", 5000),
+        (u"Panel Programmers", 5000),
+        (u"Panel Specials", 5000),
+        (u"Panel Initiatives", 5000),
+        (u"Panel Electrify", 5000),
+        (u"Panel Corporate", 5000),
+        (u"Workshop", 8000),
+    ]
 
-#     font_style = xlwt.XFStyle()
-#     font_style.font.bold = True
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
 
-#     for col_num in range(len(columns)):
-#         ws.write(row_num, col_num, columns[col_num][0], font_style)
-#         # set column width
-#         ws.col(col_num).width = columns[col_num][1]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num][0], font_style)
+        # set column width
+        ws.col(col_num).width = columns[col_num][1]
 
-#     font_style = xlwt.XFStyle()
-#     font_style.alignment.wrap = 1
+    font_style = xlwt.XFStyle()
+    font_style.alignment.wrap = 1
 
 
     
-#     for obj in queryset:
-#         lista = [ field.name for field in Event.objects.filter(event__event_part = obj.user)]
-#         listw = [ field.name for field in Workshop.objects.filter(workshop__workshop_part = obj.user)]
-#         row_num += 1
-#         row = [
-#             obj.name,
-#             obj.email,
-#             obj.mobile,
-#             obj.institute,
-#             obj.gender,
-#             obj.dob,
-#             obj.year,
-#             str(lista),
-#             str(listw)
-
-#         ]
-#         for col_num in range(len(row)):
-#             ws.write(row_num, col_num, row[col_num], font_style)
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.name,
+            obj.email,
+            obj.mobile,
+            obj.institute,
+            obj.gender,
+            obj.year,
+            obj.panel_des,
+            obj.panel_elixir,
+			obj.panel_robo,
+			obj.panel_prog,
+			obj.panel_specials,
+			obj.panel_init,
+			obj.panel_elect,
+			obj.panel_corporate,
+			obj.workshop
+        ]
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
             
-#     wb.save(response)
-#     return response
+    wb.save(response)
+    return response
 
-# export_xls.short_description = u"Export XLS"
+export_xls.short_description = u"Export XLS"
 
-# class ProfileAdmin(admin.ModelAdmin):
-#     actions = [export_xls,]
+class QuickAdmin(admin.ModelAdmin):
+    actions = [export_xls,]
 
-admin.site.register(quick)
+admin.site.register(quick, QuickAdmin)
